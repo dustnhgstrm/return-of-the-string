6f831177cc911e45d0b6f683461b9ee09bc5da72
Dustin Hagstrom <dustin.l.hagstrom@gmail.com>
2021-02-18 12:24:35 -0500

    made functions

--- main.js
+++ main.js
@@ -2,8 +2,38 @@
  * YOUR CODE HERE! *
  *******************/
 
+function yell(atSomeone){
+  return atSomeone + "!"
+}
 
+function getFirstCharacter(params0) {
+  return params0[0];
+}
 
+function getLastCharacter(params9) {
+  return params9[params9.length + -1]
+}
+
+function getOneCharacter(params1, num1) {
+  return params1[num1];
+}
+
+function getTwoCharacters(params2, num1, num2) {
+  return params2[num1] + params2[num2];
+}
+
+function makeCapitalized(params3) {
+  return params3.toUpperCase();
+}
+
+function yellLouder(params4) {
+  return params4.toUpperCase() + "!!!"
+}
+
+function getInitials(params5) {
+  const initial = params5.indexOf(' ')
+  return params5[0] + "." + params5[initial + 1] +"."
+}
 
 
 
